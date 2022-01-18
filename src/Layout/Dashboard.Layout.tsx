d88761b317a412652d5affc66f83cd9d1936d0b0
Ikhwan A Latif <ikhwanal235@gmail.com>
2022-01-18 17:00:45 +0700

    test

--- src/Layout/Dashboard.Layout.tsx
+++ src/Layout/Dashboard.Layout.tsx
@@ -1,10 +1,22 @@
+import { useLayoutEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../Components/Header.Component";
+import { modalStore } from "../store/modal.store";
 
 const Dashboard = () => {
+	const [modal, setModal] = useState<boolean>(modalStore.initValue);
+
+	useLayoutEffect(() => {
+		const subs = modalStore.subscribe(setModal);
+
+		return () => {
+			subs.unsubscribe();
+		};
+	});
+	console.log(`${modal ? "bg-gray-400" : "asd"}`);
 	return (
 		<>
-			<div className="">
+			<div className="bg-gray-400 z-40">
 				<Header />
 				<Outlet />
 			</div>
